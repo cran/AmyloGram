5b5f85d90acc1dfd59ec307cc62f865397b9c5c5
Michal Burdukiewicz <michalburdukiewicz@gmail.com>
2016-09-24 13:40:09 +0000

    version 1.0

---
+++ R/AmyloGram-package.R
@@ -0,0 +1,33 @@
+#' Prediction of amyloids
+#'
+#' Amyloids are proteins associated with the number of clinical disorders (e.g.,
+#' Alzheimer's, Creutzfeldt-Jakob's and Huntington's diseases). Despite their
+#' diversity, all amyloid proteins can undergo aggregation initiated by 6- to
+#' 15-residue segments called hot spots. Henceforth, amyloids form unique,
+#' zipper-like beta-structures, which are often harmful. To find the patterns
+#' defining the hot spots, we developed our novel predictor of amyloidogenicity
+#' AmyloGram, based on random forests.
+#'
+#' AmyloGram is available as R function (\code{\link{predict.ag_model}}) or
+#' shiny GUI (\code{\link{AmyloGram_gui}}).
+#'
+#' The package is enriched with the benchmark data set \code{\link{pep424}}.
+#'
+#' @name AmyloGram-package
+#' @aliases AmyloGram-package AmyloGram
+#' @docType package
+#' @author
+#' Maintainer: Michal Burdukiewicz <michalburdukiewicz@@gmail.com>
+#' @references Burdukiewicz MJ, Sobczyk P, Roediger S, Duda-Madej A,
+#' Mackiewicz P, Kotulska M. (2016) \emph{Prediction of amyloidogenicity
+#' based on the n-gram analysis}. PeerJ Preprints 4:e2390v1
+#' \url{https://doi.org/10.7287/peerj.preprints.2390v1}
+#' @keywords package
+#' @importFrom biogram count_multigrams decode_ngrams degenerate seq2ngrams
+#' test_features
+#' @importFrom ranger ranger
+#' @importFrom seqinr a read.fasta
+#' @importFrom shiny runApp
+#' @importFrom stats predict
+#' @importFrom utils capture.output
+NULL
---
+++ inst/AmyloGram/readme.md
@@ -0,0 +1,17 @@
+<img src="AmyloGram_logo.png" alt="logo" style="width: 200px;"/>
+
+**AmyloGram** predicts amyloidogenic sequences (hot spots) in eukaryotic proteins using [random forests](https://www.stat.berkeley.edu/~breiman/RandomForests/cc_home.htm) and [n-gram analysis](http://github.com/michbur/biogram).  
+
+**Restrictions**:
+* Be patient - calculations can take up to few minutes.  
+* Up to 100 sequences may be analyzed at the same time.  
+
+**Authors**: [Michal Burdukiewicz](http://www.smorfland.uni.wroc.pl/), [Piotr Sobczyk](http://prac.im.pwr.wroc.pl/~sobczyk/), [Malgorzata Kotulska](http://www.kotulska-lab.pwr.wroc.pl/).
+
+**Learn more**:
+* [AmyloGram preprint](https://peerj.com/preprints/2390/).  
+* [AmyloGram R package](https://github.com/michbur/AmyloGram). 
+
+**Example proteins**:
+
+
